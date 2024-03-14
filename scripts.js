e8f688d90ea414e72c7bf0234bf3af5c0ed7c9ae
ANSAR SHAIK <145973205+ansarshaik965@users.noreply.github.com>
2024-03-14 17:19:36 +0530

    Create scripts.js
    scripts added

---
+++ scripts.js
@@ -0,0 +1,47 @@
+// Add your API endpoint here
+var API_ENDPOINT = "https://csg7yjp5n4.execute-api.us-east-2.amazonaws.com/prod";
+
+// AJAX POST request to save student data
+document.getElementById("savestudent").onclick = function(){
+    var inputData = {
+        "studentid": $('#studentid').val(),
+        "name": $('#name').val(),
+        "class": $('#class').val(),
+        "age": $('#age').val()
+    };
+    $.ajax({
+        url: "https://csg7yjp5n4.execute-api.us-east-2.amazonaws.com/prod",
+        type: 'POST',
+        data:  JSON.stringify(inputData),
+        contentType: 'application/json; charset=utf-8',
+        success: function (response) {
+            document.getElementById("studentSaved").innerHTML = "Student Data Saved!";
+        },
+        error: function () {
+            alert("Error saving student data.");
+        }
+    });
+}
+
+// AJAX GET request to retrieve all students
+document.getElementById("getstudents").onclick = function(){  
+    $.ajax({
+        url: "https://csg7yjp5n4.execute-api.us-east-2.amazonaws.com/prods",
+        type: 'GET',
+        contentType: 'application/json; charset=utf-8',
+        success: function (response) {
+            $('#studentTable tr').slice(1).remove();
+            jQuery.each(response, function(i, data) {          
+                $("#studentTable").append("<tr> \
+                    <td>" + data['studentid'] + "</td> \
+                    <td>" + data['name'] + "</td> \
+                    <td>" + data['class'] + "</td> \
+                    <td>" + data['age'] + "</td> \
+                    </tr>");
+            });
+        },
+        error: function () {
+            alert("Error retrieving student data.");
+        }
+    });
+}
